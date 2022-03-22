47154b91074f46f0642bfa5378f0801b8e9961b1
muratbaseren <muratbaseren@gmail.com>
2022-03-22 22:42:23 +0300

    .

---
+++ ConsoleProgramming/ConsoleProgramming_Ders3_Part1/ConsoleProgramming_Ders3_Part1/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+
+namespace ConsoleProgramming_Ders3_Part1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = new int[3];
+            dizi[0] = 20;
+            dizi[1] = 10;
+            dizi[2] = 30;
+            //dizi[3] = 40;
+
+            Console.WriteLine("Dizi Değerleri");
+            Console.WriteLine();
+
+            //for (int i = 0; i < dizi.Length; i++)
+            //{
+            //    int item = dizi[i];
+            //    Console.WriteLine(i + ". item : " + item);
+            //}
+
+
+
+
+
+            //int[] tempDizi = new int[dizi.Length];
+
+            //for (int i = 0; i < dizi.Length; i++)
+            //{
+            //    tempDizi[i] = dizi[i];
+            //}
+
+            //// boxing
+            //object temp = tempDizi;
+
+            // unboxing = casting
+            ////int sayi = (int)temp;
+
+
+
+            //object cloneArray = dizi.Clone();
+            //int[] dizi2 = (int[])cloneArray;
+
+
+            //foreach (int item in dizi2)
+            //{
+            //    int i = Array.IndexOf(dizi, item);
+            //    Console.WriteLine(i + ". item : " + item);
+            //}
+
+
+            //int[] dizi3 = new int[dizi.Length + 2];
+            //dizi.CopyTo(dizi3, 2);
+
+
+            //for (int i = 0; i < dizi3.Length; i++)
+            //{
+            //    int item = dizi3[i];
+            //    Console.WriteLine(i + ". item : " + item);
+            //}
+
+
+
+            //Array.Sort(dizi);
+
+            //for (int i = 0; i < dizi.Length; i++)
+            //{
+            //    int item = dizi[i];
+            //    Console.WriteLine(i + ". item : " + item);
+            //}
+
+            //Console.WriteLine();
+
+            //Array.Reverse(dizi);
+
+            //for (int i = 0; i < dizi.Length; i++)
+            //{
+            //    int item = dizi[i];
+            //    Console.WriteLine(i + ". item : " + item);
+            //}
+
+
+            //Array.Resize(ref dizi, dizi.Length + 1);
+
+            //dizi[dizi.Length - 1] = 40;
+
+            //for (int i = 0; i < dizi.Length; i++)
+            //{
+            //    int item = dizi[i];
+            //    Console.WriteLine(i + ". item : " + item);
+            //}
+
+
+            Console.WriteLine("Ürün Adet Girişi");
+            Console.WriteLine("================");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Ürün adedi girerken -1 değeri girerseniz adet girişini tamamlamış olacaksınız.");
+            Console.ResetColor();
+
+            Console.WriteLine();
+
+            int[] adetler = new int[0];
+            int deger = 0;
+
+            do
+            {
+                Console.Write((adetler.Length + 1) + ". ürün adedi : ");
+                deger = int.Parse(Console.ReadLine());
+
+                if(deger > -1)
+                {
+                    Array.Resize(ref adetler, adetler.Length + 1);
+                    adetler[adetler.Length - 1] = deger;
+                }
+
+            } while (deger > -1);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Array.Sort(adetler);
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            int sayac = 0;
+            foreach (int item in adetler)
+            {
+                //int i = Array.IndexOf(adetler, item,) + 1;
+                Console.WriteLine((sayac+1) + ". ürün adedi : " + item);
+                sayac++;
+            }
+
+            Console.ResetColor();
+        }
+    }
+}
---
+++ ConsoleProgramming/ConsoleProgramming_Ders3_Part2/ConsoleProgramming_Ders3_Part2/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+namespace ConsoleProgramming_Ders3_Part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //BaslikYaz("Merhaba Dünya!");
+
+            //BaslikYaz("Console Programming");
+
+            //BaslikYaz();
+
+            //int sayi = Topla(5);
+            //Console.WriteLine(sayi);
+
+
+
+            // Array initializer..
+            //int[] sayilarim = new int[] { 4, 5, 6 };
+
+            //int toplam = Topla2(sayilarim);
+            //int toplam2 = Topla2(new int[] { 4, 5, 6 });
+
+
+            //int toplam = Topla3(3, 4, 5, 6, 7, 8, 9, 1, 1, 2232, 4456, 7);
+
+            //Console.WriteLine("Toplam : " + toplam);
+
+            // Method overloading..
+            //Console.WriteLine(Birlestir("aa", "bb"));
+            //Console.WriteLine(Birlestir("aa", "bb", "cc"));
+
+
+
+            //string ad = "Murat";
+            //Console.WriteLine(ad);
+
+
+            //char[] karakterler = new char[5];
+            //karakterler[0] = 'm';
+            //karakterler[1] = 'u';
+            //karakterler[2] = 'r';
+            //karakterler[3] = 'a';
+            //karakterler[4] = 't';
+
+            //for (int i = 0; i < karakterler.Length; i++)
+            //{
+            //    Console.Write(karakterler[i]);
+            //}
+
+
+            //string ad = "Murat;Nihal;Bora";
+            //string ad2 = ad.ToLower();
+
+            //Console.WriteLine(ad);
+            //Console.WriteLine(ad2);
+
+
+           
+
+            DateTime tarih = new DateTime(2022, 1, 30, 12, 30, 20);
+            Console.WriteLine(tarih.ToShortDateString());
+            Console.WriteLine(tarih.ToLongDateString());
+
+        }
+
+
+
+        static void BaslikYaz(string baslik = "başlık")
+        {
+            Console.WriteLine(baslik);
+
+            for (int i = 0; i < baslik.Length; i++)
+            {
+                Console.Write("=");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        static int Topla(int sayi1 = 0, int sayi2 = 0)
+        {
+            int sonuc = sayi1 + sayi2;
+            return sonuc;
+        }
+
+        static int Topla2(int[] sayilar)
+        {
+            int sonuc = 0;
+
+            //for (int i = 0; i < sayilar.Length; i++)
+            //{
+            //    sonuc += sayilar[i];
+            //}
+
+            foreach (int sayi in sayilar)
+            {
+                sonuc += sayi;
+            }
+
+            return sonuc;
+        }
+
+        static int Topla3(params int[] sayilar)
+        {
+            int sonuc = 0;
+
+            foreach (int sayi in sayilar)
+            {
+                sonuc += sayi;
+            }
+
+            return sonuc;
+        }
+
+
+        static string Birlestir(string metin1, string metin2)
+        {
+            string sonuc = metin1 + metin2;
+            return sonuc;
+        }
+
+        static string Birlestir(string metin1, string metin2, string metin3)
+        {
+            string sonuc = metin1 + metin2 + metin3;
+            return sonuc;
+        }
+    }
+}
