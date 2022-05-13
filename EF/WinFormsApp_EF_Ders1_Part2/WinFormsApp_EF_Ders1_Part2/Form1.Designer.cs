15185c105c617b4b61b01503704acfb2cf9c50e8
muratbaseren <muratbaseren@gmail.com>
2022-05-13 22:02:13 +0300

    EF Part 1

---
+++ EF/WinFormsApp_EF_Ders1_Part2/WinFormsApp_EF_Ders1_Part2/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace WinFormsApp_EF_Ders1_Part2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetAll = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.txtInsCatName = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.txtUpdCatName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGetAll
+            // 
+            this.btnGetAll.Location = new System.Drawing.Point(12, 12);
+            this.btnGetAll.Name = "btnGetAll";
+            this.btnGetAll.Size = new System.Drawing.Size(75, 23);
+            this.btnGetAll.TabIndex = 0;
+            this.btnGetAll.Text = "Get All";
+            this.btnGetAll.UseVisualStyleBackColor = true;
+            this.btnGetAll.Click += new System.EventHandler(this.btnGetAll_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(129, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(574, 160);
+            this.listBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(129, 178);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(574, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(48, 122);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 3;
+            this.btnInsert.Text = "Ekle";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // txtInsCatName
+            // 
+            this.txtInsCatName.Location = new System.Drawing.Point(12, 96);
+            this.txtInsCatName.Name = "txtInsCatName";
+            this.txtInsCatName.Size = new System.Drawing.Size(111, 20);
+            this.txtInsCatName.TabIndex = 4;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(48, 200);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // txtUpdCatName
+            // 
+            this.txtUpdCatName.Location = new System.Drawing.Point(12, 174);
+            this.txtUpdCatName.Name = "txtUpdCatName";
+            this.txtUpdCatName.Size = new System.Drawing.Size(111, 20);
+            this.txtUpdCatName.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Sil";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 339);
+            this.Controls.Add(this.txtUpdCatName);
+            this.Controls.Add(this.txtInsCatName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnGetAll);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetAll;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.TextBox txtInsCatName;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.TextBox txtUpdCatName;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EF/WinFormsApp_EF_Ders1_Part2/WinFormsApp_EF_Ders1_Part2/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp_EF_Ders1_Part2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string appName = ConfigurationManager.AppSettings["AppName"];
+            string version = ConfigurationManager.AppSettings["Version"];
+
+            Text = $"{appName} - {version}";
+        }
+
+        private void btnGetAll_Click(object sender, EventArgs e)
+        {
+            NorthwindEntities db = new NorthwindEntities();
+
+            List<Category> categories = db.Categories.ToList();
+
+            listBox1.DataSource = categories;
+            listBox1.DisplayMember = nameof(Category.Description); // CategoryName
+            listBox1.ValueMember = nameof(Category.CategoryID); // CategoryID
+
+            dataGridView1.DataSource = categories;
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            NorthwindEntities db = new NorthwindEntities();
+
+            Category category = new Category();
+            category.CategoryName = txtInsCatName.Text;
+
+            db.Categories.Add(category);
+            db.SaveChanges();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int catId = (int)dataGridView1.CurrentRow.Cells["CategoryID"].Value;
+            //int catId = (int)dataGridView1.SelectedRows[0].Cells["CategoryID"].Value;
+
+            NorthwindEntities db = new NorthwindEntities();
+
+            Category category = db.Categories.Where(c => c.CategoryID == catId).FirstOrDefault();
+            category.CategoryName = txtUpdCatName.Text;
+
+            db.SaveChanges();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int catId = (int)dataGridView1.CurrentRow.Cells["CategoryID"].Value;
+
+            NorthwindEntities db = new NorthwindEntities();
+            Category category = db.Categories.Where(c => c.CategoryID == catId).FirstOrDefault();
+
+            db.Categories.Remove(category);
+            db.SaveChanges();
+        }
+    }
+}
