ff0f485ebbb13b4dc2d160d1f1a4c8e37409a95d
muratbaseren <muratbaseren@gmail.com>
2022-05-13 22:02:13 +0300

    EF Part 1

---
+++ EF/WinFormsApp_EF_Ders1_Part1/WinFormsApp_EF_Ders1_Part1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WinFormsApp_EF_Ders1_Part1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetAll = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGetAll2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGetAll
+            // 
+            this.btnGetAll.Location = new System.Drawing.Point(12, 12);
+            this.btnGetAll.Name = "btnGetAll";
+            this.btnGetAll.Size = new System.Drawing.Size(75, 23);
+            this.btnGetAll.TabIndex = 0;
+            this.btnGetAll.Text = "Get All";
+            this.btnGetAll.UseVisualStyleBackColor = true;
+            this.btnGetAll.Click += new System.EventHandler(this.btnGetAll_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(93, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(695, 342);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 357);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // btnGetAll2
+            // 
+            this.btnGetAll2.Location = new System.Drawing.Point(12, 41);
+            this.btnGetAll2.Name = "btnGetAll2";
+            this.btnGetAll2.Size = new System.Drawing.Size(75, 23);
+            this.btnGetAll2.TabIndex = 3;
+            this.btnGetAll2.Text = "Get All 2";
+            this.btnGetAll2.UseVisualStyleBackColor = true;
+            this.btnGetAll2.Click += new System.EventHandler(this.btnGetAll2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 399);
+            this.Controls.Add(this.btnGetAll2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnGetAll);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetAll;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGetAll2;
+    }
+}
---
+++ EF/WinFormsApp_EF_Ders1_Part1/WinFormsApp_EF_Ders1_Part1/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp_EF_Ders1_Part1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetAll_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Server=(localdb)\\MSSQLLocalDB;Database=Northwind;Trusted_Connection=True");
+            SqlCommand command = new SqlCommand("SELECT CategoryID, CategoryName FROM Categories", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            dataGridView1.DataSource = dataTable;
+
+            //int rowCount = dataTable.Rows.Count;
+            //label1.Text = $"Kayıt Sayısı : {rowCount}"; // string interpolation
+
+            int rowCount = 0;
+            int idTotal = 0;
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                rowCount++;
+                idTotal += (int)row["CategoryID"];
+            }
+
+            label1.Text = $"Kayıt Sayısı : {rowCount} - ID Total : {idTotal}"; // string interpolation
+
+            //command.CommandText = "SELECT COUNT(*) FROM Categories";
+            //connection.Open();
+
+            //int rowCount = (int)command.ExecuteScalar();
+
+            //connection.Close();
+
+            //label1.Text = $"Kayıt Sayısı : {rowCount}"; // string interpolation
+        }
+
+        private void btnGetAll2_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection("Server=(localdb)\\MSSQLLocalDB;Database=Northwind;Trusted_Connection=True");
+            SqlCommand command = new SqlCommand("SELECT CategoryID, CategoryName, Description FROM Categories", connection);
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+
+            DataTable dataTable = new DataTable();
+            adapter.Fill(dataTable);
+
+            List<Category> categories = new List<Category>();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Category category = new Category();
+                category.Id = (int)row["CategoryID"];
+                category.Name = (string)row["CategoryName"];
+                category.Desc = (string)row["Description"];
+
+                categories.Add(category);
+            }
+
+            Category category1 = categories[0];
+            //MessageBox.Show(category1.Name);
+            //MessageBox.Show(category1.Desc);
+            int ids = 0;
+
+            foreach (Category item in categories)
+            {
+                if (item.Name.StartsWith("C"))
+                {
+                    ids += item.Id;
+                }
+            }
+
+            dataGridView1.DataSource = categories;
+
+        }
+    }
+}
